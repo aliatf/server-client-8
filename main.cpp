812d75b9373b436646b068acae45211149d76ff9
aliatf <49996254+aliatf@users.noreply.github.com>
2019-04-27 15:39:23 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <winsock2.h> // Wincosk2.h должен быть раньше windows!
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include "Server.h"
+
+
+int main()
+{
+	Server server;
+	server.handle();
+	
+}
